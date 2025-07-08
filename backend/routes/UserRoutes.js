0f5b18cc93ef49aea7f664c9516790489f95fc5b
EnzoRicardo <enzoricardodeoliveira@gmail.com>
2025-07-08 18:56:52 -0300

    validando os usuarios

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,13 +1,14 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 //Controle
-const {register} = require("../controllers/UserController")
+const {register} = require("../controllers/UserController");
 
 //Middlewares
-const validate = require("../middlewares/handleValidation")
+const validate = require("../middlewares/handleValidation");
+const {userCreateValidation} = require("../middlewares/userValidations");
 
 //Rotas
-router.post("/register", validate, register);
+router.post("/register", userCreateValidation(), validate, register);
 
 module.exports = router;
