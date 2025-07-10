af336d6f44cb69d14c6218beb60872f5dcc4f790
EnzoRicardo <enzoricardodeoliveira@gmail.com>
2025-07-10 14:59:15 -0300

    criando rota para deletar fotos

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -24,7 +24,8 @@ const insertPhoto = async(req, res) => {
     if(!newPhoto) {
         res.status(422).json({
             errors: ["Houve um problema, por favor tente novamente mais tarde."],
-        })
+        });
+        return;
     }
 
     res.status(201).json(newPhoto);
@@ -33,6 +34,43 @@ const insertPhoto = async(req, res) => {
 };
 
 
+//Remover foto do DB
+const deletePhoto = async(req, res) => {
+
+    const {id} = req.params
+
+    const reqUser = req.user
+
+    try {
+        const photo = await Photo.findById(mongoose.Types.ObjectId(id))
+
+        //checar se a foto existe
+        if (!photo) {
+            res.status(404).json({erros: ["Foto não encontrada!"]});
+            return;
+        }
+
+        //checar se a foto pertence ao usuario
+        if (!photo.userId.equals(reqUser._id)) {
+            res
+                .status(422)
+                .json({
+                    errors: ["Ocorreu um erro, por favor tente novamente mais tarde!"],
+                });
+        }
+
+        await Photo.findByIdAndDelete(photo._id)
+
+        res
+            .status(200)
+            .json({id: photo._id, message:"Foto excluída com sucesso!"});
+    } catch (error) {
+        res.status(404).json({ errors: ["Foto não encontrada!"]});
+        return;
+    }
+};
+
 module.exports = {
     insertPhoto,
+    deletePhoto,
 };
